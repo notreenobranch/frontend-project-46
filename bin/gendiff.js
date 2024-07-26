2a522c844ef04ad355d92b4ce21ef50386bfc2f5
Nadya K. <notreenobranch@proton.me>
2024-07-26 15:36:03 +1100

    Update /bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,8 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'output usage information');
 
 program.parse();
