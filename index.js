d74cd141d7f57e299ef061a5a6691c4ed61de6cc
Nadya K. <notreenobranch@proton.me>
2024-07-28 17:06:00 +1100

    Add an ability to read files content

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const gendiff = (filepath1, filepath2) => {
-  console.log(`hello, ${filepath1} and ${filepath2}`);
+import path from 'path';
+import fs from 'fs';
+
+const readFile = (filepath) => {
+  const absolutePath = path.resolve(process.cwd(), filepath);
+  const data = fs.readFileSync(absolutePath);
+  return data;
 };
 
-export default gendiff;
+export default (filepath1, filepath2) => {
+  const data1 = readFile(filepath1);
+  const data2 = readFile(filepath2);
+
+  console.log(`data1:\n${data1}\ndata2:\n${data2}`);
+};
