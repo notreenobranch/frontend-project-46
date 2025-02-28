71c7560cb3e7f95fd0c8927012218b47d6845ed3
Nadya K. <notreenobranch@proton.me>
2025-02-28 18:28:00 +1100

    Update gendiff -h output

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,8 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
